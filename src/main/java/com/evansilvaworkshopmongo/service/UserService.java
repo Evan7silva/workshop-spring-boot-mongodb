090df6a8c33ec4cdf5163a2b4f7d7e6edebb23a9
Evan7silva <evanildosilva467@gmail.com>
2022-06-15 14:00:58 -0300

    Conectando ao MongoDB

---
+++ src/main/java/com/evansilvaworkshopmongo/service/UserService.java
@@ -0,0 +1,21 @@
+package com.evansilvaworkshopmongo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.evansilvaworkshopmongo.domain.User;
+import com.evansilvaworkshopmongo.repository.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repo;
+	
+	public List<User> findAll(){
+		return repo.findAll();
+		
+	}
+}
