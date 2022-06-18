59e8ee2f0e211866b082ab488fc4100c6e9ecafe
Evan7silva <evanildosilva467@gmail.com>
2022-06-18 15:30:46 -0300

    Obtendo um usuário por id

--- src/main/java/com/evansilvaworkshopmongo/service/UserService.java
+++ src/main/java/com/evansilvaworkshopmongo/service/UserService.java
@@ -1,12 +1,14 @@
 package com.evansilvaworkshopmongo.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.evansilvaworkshopmongo.domain.User;
 import com.evansilvaworkshopmongo.repository.UserRepository;
+import com.evansilvaworkshopmongo.service.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,6 +18,18 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repo.findAll();
-		
 	}
+	
+//	public User findById(String id) {
+//		User user = repo.find
+//		if(user == null) {
+//			throw new ObjectNotRoundException("Object not found");
+//		}
+//		return user;
+//		}
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+			
+		}
 }
