6930fac97f89fa2b1c06adcd39669cb59668e3b5
Evan7silva <evanildosilva467@gmail.com>
2022-06-21 13:52:53 -0300

    Projeção dos dados do autor com DTO

--- src/main/java/com/evansilvaworkshopmongo/config/Instantiation.java
+++ src/main/java/com/evansilvaworkshopmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.evansilvaworkshopmongo.domain.Post;
 import com.evansilvaworkshopmongo.domain.User;
+import com.evansilvaworkshopmongo.dto.AuthorDTO;
 import com.evansilvaworkshopmongo.repository.PostRepository;
 import com.evansilvaworkshopmongo.repository.UserRepository;
 
@@ -32,14 +33,16 @@ public class Instantiation implements CommandLineRunner{
 		userRepository.deleteAll();
 		postRepository.deleteAll();
 		
+		
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para sao paulo, Abraços", maria);
-		Post post2 = new Post(null, sdf.parse("21/03/2018"), "Bom dia", "Acordei feliz hoje", maria);
-		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para sao paulo, Abraços", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("21/03/2018"), "Bom dia", "Acordei feliz hoje", new AuthorDTO(maria));
+		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 
--- src/main/java/com/evansilvaworkshopmongo/domain/Post.java
+++ src/main/java/com/evansilvaworkshopmongo/domain/Post.java
@@ -7,6 +7,8 @@ import java.util.Objects;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import com.evansilvaworkshopmongo.dto.AuthorDTO;
+
 @Document
 public class Post implements Serializable{
 
@@ -17,14 +19,14 @@ public class Post implements Serializable{
 	private Date date;
 	private String title;
 	private String boby;
-	private User author;
+	private AuthorDTO author;
 
 	
 	public Post() {
 		
 	}
 
-	public Post(String id, Date date, String title, String boby,User author) {
+	public Post(String id, Date date, String title, String boby,AuthorDTO author) {
 		super();
 		this.id = id;
 		this.date = date;
@@ -70,11 +72,11 @@ public class Post implements Serializable{
 		return Objects.hash(id);
 	}
 	
-	public User getAuthor() {
+	public AuthorDTO getAuthor() {
 		return author;
 	}
 
-	public void setAuthor(User author) {
+	public void setAuthor(AuthorDTO author) {
 		this.author = author;
 	}
 
