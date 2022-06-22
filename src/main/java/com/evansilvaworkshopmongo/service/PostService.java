f3a8878eddc9fab903b2312cc42b74854f20a275
Evan7silva <evanildosilva467@gmail.com>
2022-06-21 21:00:53 -0300

    Obtendo um post por id

---
+++ src/main/java/com/evansilvaworkshopmongo/service/PostService.java
@@ -0,0 +1,31 @@
+package com.evansilvaworkshopmongo.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.evansilvaworkshopmongo.domain.Post;
+import com.evansilvaworkshopmongo.repository.PostRepository;
+import com.evansilvaworkshopmongo.service.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	
+	
+//	public User findById(String id) {
+//		User user = repo.find
+//		if(user == null) {
+//			throw new ObjectNotRoundException("Object not found");
+//		}
+//		return user;
+//		}
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+			
+		}
+}
