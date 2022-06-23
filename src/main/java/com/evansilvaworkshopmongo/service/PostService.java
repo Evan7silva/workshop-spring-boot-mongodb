c53ce75b1fb34905e5073fe93f863834400847a2
Evan7silva <evanildosilva467@gmail.com>
2022-06-23 14:34:35 -0300

    Consulta simples com query methods

--- src/main/java/com/evansilvaworkshopmongo/service/PostService.java
+++ src/main/java/com/evansilvaworkshopmongo/service/PostService.java
@@ -1,5 +1,6 @@
 package com.evansilvaworkshopmongo.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,4 +29,8 @@ public class PostService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
 			
 		}
+	
+	public List<Post> findByTitle(String text){
+		return repo.findByTitleContainingIgnoreCase(text);
+	}
 }
